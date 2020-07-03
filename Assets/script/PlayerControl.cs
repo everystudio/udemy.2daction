ce8aefceebb8815f78ac05e9510d6edc386f39c1
anogame_master <anogame.master@gmail.com>
2020-07-03 14:07:33 +0900

    アニメーション追加してみた

---
+++ Assets/script/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float move_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float move_x = Input.GetAxis("Horizontal");
+
+        gameObject.GetComponent<Rigidbody2D>().velocity =
+            new Vector2(
+                move_x * move_speed,
+                gameObject.GetComponent<Rigidbody2D>().velocity.y
+                );
+
+
+       if(Input.GetButton("Jump"))
+		{
+            Debug.Log("jump");
+		}
+        
+    }
+}
