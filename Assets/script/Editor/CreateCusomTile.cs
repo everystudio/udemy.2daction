f8c1f2652f0331e000828b1a9c8885ff557ec74a
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-07-03 13:41:42 +0900

    ファイル追加など

---
+++ Assets/script/Editor/CreateCusomTile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateCusomTile : Editor
+{
+    [MenuItem("ScriptableObjectTest/Create")]
+    public static void Create()
+    {
+        // ScriptableObject.CreateInstance()でインスタンスを生成
+        // この時点ではアセット化はされていない
+        var asset = CreateInstance<CustomTile>();
+
+        // アセット化するにはAssetDatabase.CreateAsset()
+        // 拡張子は必ず.assetとする
+        AssetDatabase.CreateAsset(asset, "Assets/customtile.asset");
+        AssetDatabase.Refresh();
+    }
+
+}
