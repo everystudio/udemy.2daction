d28b78f324aa79619341a84445c1aa51efeedacc
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-07-06 15:36:21 +0900

    キャラの向きやアニメーション制御部分を追加

--- Assets/script/PlayerControl.cs
+++ Assets/script/PlayerControl.cs
@@ -14,17 +14,33 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float move_x = Input.GetAxis("Horizontal");
-
         gameObject.GetComponent<Rigidbody2D>().velocity =
             new Vector2(
                 move_x * move_speed,
                 gameObject.GetComponent<Rigidbody2D>().velocity.y
                 );
 
+        // 移動量に応じてアニメーションさせる
+        gameObject.GetComponent<Animator>().SetFloat("move" , Mathf.Abs(move_x));
 
-       if(Input.GetButton("Jump"))
+        if (move_x != 0.0f)
+        {
+            float chara_dir = 1.0f;
+            if( move_x < 0.0f)
+            {
+                chara_dir = -1.0f;
+            }
+            gameObject.transform.localScale = new Vector3(
+                chara_dir,
+                1.0f,
+                1.0f
+                );
+        }
+
+
+        // ジャンプ処理
+        if(Input.GetButton("Jump"))
 		{
             Debug.Log("jump");
 		}
